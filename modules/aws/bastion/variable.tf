1f72f70babd275cfb46ff58fddbc749293d288e5
keitakn <keita.koga.work@gmail.com>
2018-12-18 23:24:59 +0900

    :recycle: #5 AMIイメージを最新状態に更新

--- modules/aws/bastion/variable.tf
+++ modules/aws/bastion/variable.tf
@@ -6,7 +6,7 @@ variable "bastion" {
     stg.name              = "stg-bastion"
     dev.name              = "dev-bastion"
     qa.name               = "qa-bastion"
-    default.ami           = "ami-00f9d04b3b3092052"
+    default.ami           = "ami-00a5245b4816c38e6"
     default.instance_type = "t3.micro"
     default.volume_type   = "gp2"
     default.volume_size   = "30"
